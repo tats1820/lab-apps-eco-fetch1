18436ad5a39a62065824e502290ac0a3a4bbee65
David Giraldo M <davidgiraldom@Davids-MacBook-Pro.local>
2022-08-04 21:07:31 -0500

    Setting files

---
+++ public/sketch.js
@@ -0,0 +1,31 @@
+let canvas;
+
+function setup() {
+    frameRate(60);
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.style('z-index', '-1');
+    canvas.style('position', 'fixed');
+    canvas.style('top', '0');
+    canvas.style('right', '0');
+}
+
+function draw() {
+    //background(0, 50);
+    background(0);
+    newCursor();
+    
+}
+
+function mouseClicked(){
+
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function newCursor() {
+    noStroke();
+    fill(255);
+    ellipse(pmouseX, pmouseY, 10, 10);
+}
